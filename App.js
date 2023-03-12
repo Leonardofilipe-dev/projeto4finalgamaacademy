ea50064188271fa643080f482f73a43b93abadb9
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-03-11 22:05:47 -0300

    Inclusão de CRUD Categorias

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import express from "express"
 
 import routes from "./src/routes/index.js"
+import db from "./src/config/db.js";
+
+db.on("error", console.log.bind(console, "Erro ao conectar ao Mongo"))
+db.once("open", () =>{
+    console.log("Conectado com sucesso!")
+})
 
 const app = express();
 routes(app)
--- src/controller/CategoriaController.js
+++ src/controller/CategoriaController.js
@@ -1,24 +1,71 @@
+import Categoria from "../models/Categoria.js"
+
 class CategoriaController {
     static async listar(req, res) {
-        
-        res.send("Listar")
+    const listaDeCategorias = await Categoria.find()
+        res.json(listaDeCategorias)
+        try {
+            let categorias = await listar();
 
+            return res.status(200).json(categorias);
+        } catch (error) {
+            return res.status(500).json({error});
+        }
+       
+            
+        
     }
+   
+
 
     static async buscaPorId(req, res) {
-        res.send("BuscarPorId")
+        const id = req.params.id; // assume que o ID da categoria está presente nos parâmetros da requisição
+        try {
+            const categoria = await Categoria.findById( id );
+            return res.status(200).json(categoria);
+        } catch (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+    
     }
 
     static async cadastrar(req, res) {
-        res.send("Cadastrar")
+        const {nome } = req.body
+        let novaCategoria = new Categoria({
+        nome
+    });
+
+    const resultado = await novaCategoria.save();
+
+    res.json(resultado)
+
     }
 
     static async deletar(req, res) {
-        res.send("deletar")
+        const id = req.params.id
+        try {
+            const categoriaDeletada = await Categoria.findByIdAndDelete(id);
+            return res.status(200).json(categoriaDeletada);
+        } catch (err) {
+            return res.status(500).json({ error: err.message });
+        }
     }
 
     static async atualizar(req, res) {
-        res.send("atualizar")
+        const id = req.params.id
+        const {nome } = req.body
+
+        try {
+            
+            const categoriaAtualizada = await Categoria.findByIdAndUpdate(id, {nome:nome});
+
+            
+            return res.status(200).json(categoriaAtualizada);
+        } catch (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
 
     }
 
